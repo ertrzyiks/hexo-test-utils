54c3d9bc01961b0eda99344b25001d7d520976ef
ertrzyiks <mateusz.derks@gmail.com>
2018-04-03 22:17:56 +0200

    Fix hasRoute for removed routes

---
+++ specs/routing/has_route.spec.js
@@ -0,0 +1,24 @@
+import test from 'ava'
+import Hexo from 'hexo'
+import {createSandbox} from '../../lib/'
+import {hasRoute} from '../../lib/routing/'
+
+const sandbox = createSandbox(Hexo)
+
+test(async t => {
+  const ctx = await sandbox()
+  t.is(hasRoute(ctx, 'some-route'), false)
+});
+
+test(async t => {
+  const ctx = await sandbox()
+  ctx.route.set('some-route', 'Content here')
+  t.is(hasRoute(ctx, 'some-route'), true)
+});
+
+test(async t => {
+  const ctx = await sandbox()
+  ctx.route.set('some-route', 'Content here')
+  ctx.route.remove('some-route')
+  t.is(hasRoute(ctx, 'some-route'), false)
+});
