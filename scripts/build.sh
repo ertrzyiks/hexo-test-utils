3c10c384a3760e8b23dc772b4837e4a4689886ac
ertrzyiks <mateusz.derks@gmail.com>
2021-11-22 21:20:15 +0100

    Fix build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -11,7 +11,6 @@ npx babel lib --out-dir $OUTPUT_FOLDER
 
 # Copy required files
 cp package.json $OUTPUT_FOLDER
-cp package-lock.json $OUTPUT_FOLDER
 cp README.md $OUTPUT_FOLDER
 cp LICENSE $OUTPUT_FOLDER
 cp -r lib/fixtures $OUTPUT_FOLDER/fixtures
